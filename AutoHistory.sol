4b985e6cf5d534856e56d67d38e39a1b8f7d3996
Adrian <adrian.grigorov@yahoo.com>
2021-02-07 17:32:07 +0200

    po4ti e gotovo

--- AutoHistory.sol
+++ AutoHistory.sol
@@ -40,6 +40,7 @@ pragma abicoder v2;
         }
         carRepair.price = price;
         carRepair.approved = false;
+        msg.sender.call()
     }
     
     
@@ -49,9 +50,10 @@ contract AutoHistory {
     struct Car {
        string vin;
        uint256 kilometers;
-      Repair[] repairs;
-       Crash[] crashes;
        address owner;
+       Repair[] repairs;
+       Crash[] crashes;
+       Part[] parts;
     }
     
     struct Repair {
@@ -64,6 +66,20 @@ contract AutoHistory {
         string description;
         // TODO: add damaged parts
     }
+    
+    struct Part {
+        string id;
+        string name;
+        uint dateMounted;
+        uint lastRepaired; // Will this be necessary?
+    }
+    
+    event oldPartFound(string vin, Part part);
+    
+    string[] private defaultParts = [
+        "engine", "brakes", "ignition", "tyres", "suspension", 
+        "heat pump", "mirrors", "headlights", "headlights", "taillights"
+    ]; // will be assigned to every newly added car
     
     mapping (string => Car) private cars;
     
@@ -89,6 +105,20 @@ contract AutoHistory {
         Car storage newCar = cars[vin];
         newCar.vin = vin;
         newCar.kilometers = kilometers;
+        newCar.owner = msg.sender;
+        
+        for(uint i = 0; i < defaultParts.length; i++) {
+            // TODO: how to fill id and date mounted?
+            // Date mounted - for now we assume the car is new and use current date
+            
+            Part memory newPart;
+            newPart.id = "sample id";
+            newPart.name = defaultParts[i];
+            newPart.dateMounted = block.timestamp;
+            newPart.lastRepaired = 0; // default value - means no repairs have been made
+            
+            newCar.parts.push(newPart);
+        }
     }
     
     function addRepair(string memory vin, uint256 price, string memory description) public {
@@ -107,6 +137,44 @@ contract AutoHistory {
         require(carExists(vin), "Car does not exist");
         
         cars[vin].kilometers = kilometers;
+    }
+    
+    /**
+     * Check if any part of a car has not been changed or repaired for a long time
+     * Emit events if there is anything irregullar
+     */
+    function checkParts(string memory vin) public view returns (string[] memory) {
+        require(carExists(vin), "Car does not exist");
+        
+        uint oldPartsCount = 0;
+              
+        for(uint i = 0; i < cars[vin].parts.length; i++) {
+            // 157680000 = 5 years in seconds
+            if (
+                block.timestamp - cars[vin].parts[i].dateMounted > 157680000 || 
+                cars[vin].parts[i].lastRepaired != 0 &&
+                block.timestamp - cars[vin].parts[i].lastRepaired > 157680000
+            ) {
+                oldPartsCount++;
+            }
+        }
+        
+        string[] memory oldParts = new string[](oldPartsCount);
+        
+        for(uint i = 0; i < cars[vin].parts.length; i++) {
+            // 157680000 = 5 years in seconds
+            if (
+                block.timestamp - cars[vin].parts[i].dateMounted > 157680000 || 
+                cars[vin].parts[i].lastRepaired != 0 &&
+                block.timestamp - cars[vin].parts[i].lastRepaired > 157680000
+            ) {
+                // if a part hasn't been repaired in 5 years
+                oldParts[i] = cars[vin].parts[i].name;
+                //emit oldPartFound(vin, cars[vin].parts[i]);
+            }
+        }
+        
+        return oldParts;
     }
 }
 
