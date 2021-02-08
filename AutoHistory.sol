c9e6badc42e8da7b348eb2262eaf6bc822d2cf17
nikolay <nikolayynayydenov@gmail.com>
2021-02-08 18:28:39 +0200

    restore AutoHistory state

--- AutoHistory.sol
+++ AutoHistory.sol
@@ -12,7 +12,8 @@ library Shared {
      struct Part {
         string id;
         string name;
-        string dateMounted;
+        uint256 dateMounted;
+        uint256 lastRepairKilometers;
     }
     
     struct CarRepair {
@@ -55,6 +56,7 @@ contract CarRepairContract {
     
     event addNewRepairEvent(address carService, address car, Shared.Part[] parts, uint256 price);
     
+    // TODO: update repaired parts' lastRepairKilometers property
     function addNewRepair(address carAddress, Shared.Part[] memory parts, uint256 price) public payable {
         require(repairs[carAddress].carService == address(0x0), "There is a car repair for that car!");
         require(msg.value == carServiceStake, "You should send car service stake!");
@@ -134,17 +136,11 @@ contract CarRepairContract {
 
 contract AutoHistory {
     struct Car {
-       string vin;
-       uint256 kilometers;
-       address owner;
-       Repair[] repairs;
-       Crash[] crashes;
-       Part[] parts;
-    }
-    
-    struct Repair {
-        uint256 price;
-        string description;
+        bool exists; // used to check whether the car exists
+        uint256 kilometers;
+        Shared.CarRepair[] repairs;
+        Crash[] crashes;
+        Shared.Part[] parts;
     }
     
     struct Crash {
@@ -153,93 +149,78 @@ contract AutoHistory {
         // TODO: add damaged parts
     }
     
-    struct Part {
-        string id;
-        string name;
-        uint dateMounted;
-        uint lastRepaired; // Will this be necessary?
-    }
-    
-    event oldPartFound(string vin, Part part);
+    //event oldPartFound(address adr, Shared.Part part);
     
     string[] private defaultParts = [
         "engine", "brakes", "ignition", "tyres", "suspension", 
         "heat pump", "mirrors", "headlights", "headlights", "taillights"
     ]; // will be assigned to every newly added car
     
-    mapping (string => Car) private cars;
+    mapping (address => Car) private cars;
     
     /**
     * Get a car's repair + crash history
     * 
     * TODO: get crash history
     */
-    function getHistory(string memory vin) view public returns (Repair[] memory) {
-        require(carExists(vin), "Car does not exist");
+    function getHistory() view public returns (Shared.CarRepair[] memory, Crash[] memory) {
+        require(carExists(msg.sender), "Car does not exist");
         
-        return cars[vin].repairs;
+        return (cars[msg.sender].repairs, cars[msg.sender].crashes);
     
     }
      
-    function carExists(string memory vin) private view returns (bool) {
-        return bytes(cars[vin].vin).length > 0;
+    function carExists(address adr) private view returns (bool) {
+        return cars[adr].exists;
     }
      
-    function addCar(string memory vin, uint256 kilometers) public {
-        require(!carExists(vin), "Car already exists");
+    function addCar(uint256 kilometers) public {
+        require(!carExists(msg.sender), "Car already exists");
          
-        Car storage newCar = cars[vin];
-        newCar.vin = vin;
+        Car storage newCar = cars[msg.sender];
+        newCar.exists = true;
         newCar.kilometers = kilometers;
-        newCar.owner = msg.sender;
         
         for(uint i = 0; i < defaultParts.length; i++) {
             // TODO: how to fill id and date mounted?
             // Date mounted - for now we assume the car is new and use current date
             
-            Part memory newPart;
+            Shared.Part memory newPart;
             newPart.id = "sample id";
             newPart.name = defaultParts[i];
             newPart.dateMounted = block.timestamp;
-            newPart.lastRepaired = 0; // default value - means no repairs have been made
+            newPart.lastRepairKilometers = 0; // default value - means no repairs have been made
             
             newCar.parts.push(newPart);
         }
     }
     
-    function addRepair(string memory vin, uint256 price, string memory description) public {
-        require(carExists(vin), "Car does not exist");
+    function addCrash(string memory dateTime, string memory description) public {
+        require(carExists(msg.sender), "Car does not exist");
         
-        cars[vin].repairs.push(Repair(price, description));
-    }
-    
-    function addCrash(string memory vin, string memory dateTime, string memory description) public {
-        require(carExists(vin), "Car does not exist");
-        
-        cars[vin].crashes.push(Crash(dateTime, description));
+        cars[msg.sender].crashes.push(Crash(dateTime, description));
     }
      
-    function setKilometers(string memory vin, uint256 kilometers) public {
-        require(carExists(vin), "Car does not exist");
+    function setKilometers(uint256 kilometers) public {
+        require(carExists(msg.sender), "Car does not exist");
         
-        cars[vin].kilometers = kilometers;
+        cars[msg.sender].kilometers = kilometers;
     }
     
     /**
      * Check if any part of a car has not been changed or repaired for a long time
      * Emit events if there is anything irregullar
      */
-    function checkParts(string memory vin) public view returns (string[] memory) {
-        require(carExists(vin), "Car does not exist");
+    function checkParts() public view returns (string[] memory) {
+        require(carExists(msg.sender), "Car does not exist");
         
         uint oldPartsCount = 0;
               
-        for(uint i = 0; i < cars[vin].parts.length; i++) {
-            // 157680000 = 5 years in seconds
+        for(uint i = 0; i < cars[msg.sender].parts.length; i++) {
             if (
-                block.timestamp - cars[vin].parts[i].dateMounted > 157680000 || 
-                cars[vin].parts[i].lastRepaired != 0 &&
-                block.timestamp - cars[vin].parts[i].lastRepaired > 157680000
+                cars[msg.sender].kilometers - cars[msg.sender].parts[i].lastRepairKilometers > 50000 || 
+                cars[msg.sender].parts[i].lastRepairKilometers != 0 &&
+                cars[msg.sender].kilometers - cars[msg.sender].parts[i].lastRepairKilometers > 50000
             ) {
                 oldPartsCount++;
             }
@@ -247,16 +228,14 @@ contract AutoHistory {
         
         string[] memory oldParts = new string[](oldPartsCount);
         
-        for(uint i = 0; i < cars[vin].parts.length; i++) {
-            // 157680000 = 5 years in seconds
+        for(uint i = 0; i < cars[msg.sender].parts.length; i++) {
             if (
-                block.timestamp - cars[vin].parts[i].dateMounted > 157680000 || 
-                cars[vin].parts[i].lastRepaired != 0 &&
-                block.timestamp - cars[vin].parts[i].lastRepaired > 157680000
+                cars[msg.sender].kilometers - cars[msg.sender].parts[i].lastRepairKilometers > 50000 || 
+                cars[msg.sender].parts[i].lastRepairKilometers != 0 &&
+                cars[msg.sender].kilometers - cars[msg.sender].parts[i].lastRepairKilometers > 50000
             ) {
-                // if a part hasn't been repaired in 5 years
-                oldParts[i] = cars[vin].parts[i].name;
-                //emit oldPartFound(vin, cars[vin].parts[i]);
+                oldParts[i] = cars[msg.sender].parts[i].name;
+                //emit oldPartFound(msg.sender, cars[vin].parts[i]);
             }
         }
         
